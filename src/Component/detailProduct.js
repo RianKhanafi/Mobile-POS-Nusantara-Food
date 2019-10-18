f4d141ac938fb5b00a42dc94b26aebc7cc538451
RianKhanafi <riankhanafi69@gmail.com>
2019-10-18 11:05:46 +0700

    menambah detail priduct

--- src/Component/detailProduct.js
+++ src/Component/detailProduct.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react'
-import { styleSheet } from 'react-native'
-import { View, Text, Col, Grid, Container, Content } from 'native-base'
-
+import { StyleSheet,Image ,TouchableOpacity} from 'react-native'
+import { View, Text, Col, Grid, Container, Content,Button  } from 'native-base'
+import rupiahFormat from 'rupiah-format'
+const URL = 'http://192.168.1.5:5000'
 export default class detailProduct extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            product: this.props.navigation.getParam('product')
+        }
+    }
     render() {
+        // console.log(this.state.product.image)
         return (
             <>
                 <Container>
                     <Content>
                         <View style={{ flexDirection: "row", flex: 1 }}>
-                            <View style={{ flex: 1 }}>
-                                <Text> mxbcndsmcb</Text>
+                            <View style={style.viewImage}>
+                            <Image source={{ uri: `${URL}/images/${this.state.product.image}` }} style={{ width: 420, height: 420, resizeMode: 'cover'}} />
                             </View>
                         </View>
                         <View style={{ flexDirection: "row", flex: 1 }}>
-                            <View style={{ flex: 1 }}>
-                                <Text> mxbcndsmcb</Text>
+                            <View style={style.viewKeterangan}>
+                            <View style={{ flexDirection: "row", flex: 1 }}>
+                                <View style={{flex:2}}>
+                                     <Text style={{fontSize:35, color:'#3b5bb2'}}>{this.state.product.name}</Text>
+                                </View>
+                                <View style={{flex:1}}>
+                                <Text style={{fontSize:20, color:'#3b5bb2', marginTop:5}}>{rupiahFormat.convert(this.state.product.price)}</Text>
+                                </View>
+                                
+                            </View>
+                            <TouchableOpacity   style={style.button}
+                                 onPress={()=>this.props.navigation.navigate('DetailProduct', {
+                                            product:{
+                                                    id:this.state.product.id,
+                                                    name:this.state.product.name,
+                                                    description:this.state.product.description,
+                                                    image:this.state.product.image,
+                                                    price:this.state.product.price,
+                                                    quantity:this.state.product.quantity
+                                                }
+                                             })
+                                      }>
+                                            <Text>Add To cart</Text>
+                                          </TouchableOpacity>
+                              {/* <Button full primary style={style.button} onPress={this.handleLogin}>
+                              
+                            </Button> */}
                             </View>
                         </View>
                     </Content>
@@ -24,3 +57,26 @@ export default class detailProduct extends Component {
         )
     }
 }
+
+const style = StyleSheet.create({
+    viewImage: {
+        flex: 1,
+        height: 500,
+        backgroundColor: '#3b5bb2'
+    },
+    viewKeterangan: {
+        flex: 1,
+        height: 200,
+        backgroundColor: '#fff',
+        marginTop: -100,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        padding: 20
+    }, button: {
+        borderRadius: 30,
+        marginTop: 50,
+        // shadowOffset: { height: 0, width: 0 },
+        // shadowOpacity: 0,
+        elevation: 0,
+    },
+})
